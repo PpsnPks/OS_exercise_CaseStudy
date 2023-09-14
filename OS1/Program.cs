092e8fafe9f2a073dbf56d449bd7e07be4366293
Ppsn_pks <88644557+PpsnPks@users.noreply.github.com>
2023-09-14 12:55:55 +0700

    Add files via upload

---
+++ OS1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+// namespace Lab_OS_Concurrency
+// {
+//     class Program
+//     {
+//         static void TestThread1()
+//         {
+//             int i;
+//             for (i = 0; i < 10; i++)
+//                 Console.WriteLine("Thread# 1 i={0}",i);
+//         }
+//         static void TestThread2()
+//         {
+//             int i;
+//             for (i = 0; i < 10; i++)
+//                 Console.WriteLine("Thread# 2 i={0}",i);
+//         }
+//         static void Main(string[] args)
+//         {
+//             Thread th1 = new Thread(TestThread1);
+//             Thread th2 = new Thread(TestThread2);
+//             th1.Start();
+//             th2.Start();
+//         }
+//     }
+// }
+namespace Lab_OS_Concurrency02
+{
+    class Program
+    {
+        static int resource =10000;
+        static void TestThread1()
+        {
+            resource = 55555;
+        }
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(TestThread1);
+            th1.Start();
+            //Thread.Sleep(10000);
+            Console.WriteLine("resource={0}",resource);
+        }
+    }
+}
+
+// namespace Lab_OS_Concurrency01  Ex2
+// {
+//     class Program
+//     {
+//         static int resource =9999;
+//         static void TestThread1()
+//         {
+//             Console.WriteLine("Thread# 1 i={0}",resource);
+//         }
+//         static void TestThread2()
+//         {
+//             Console.WriteLine("Thread# 2 i={0}",resource);
+//         }
+//         static void Main(string[] args)
+//         {
+//             Thread th1 = new Thread(TestThread1);
+//             Thread th2 = new Thread(TestThread2);
+//             th1.Start();
+//             th2.Start();
+//         }
+//     }
+// }
